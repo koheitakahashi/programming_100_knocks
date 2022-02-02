3e6c59524e47fa1d15fdce8bdec12d9bbdaeb802
koheitakahashi <kouhei.takahashi@everyleaf.com>
2022-02-02 22:19:05 +0900

    [wip] doing 83

--- 83/knock83.rb
+++ 83/knock83.rb
@@ -1,42 +1,22 @@
-class Knock82
-  STEP_NUMBER = 15
-  FIRST_STEP_ELEMENT = [1].freeze
-  FIRST_STEP_COUNT = 0
-  FIRST_AND_LAST_STEP_ELEMENT_IN_STEP = 1
-  PRECEDING_STEP_COUNT = 1
+class Knock83
+  HANDS = {
+    0 => 'グー'.freeze,
+    1 => 'チョキ'.freeze,
+    2 => 'パー'.freeze,
+  }.freeze
 
   def self.execute
     new.execute
   end
 
-  def initialize
-    @numbers = []
-  end
-
   def execute
-    STEP_NUMBER.times do |i|
-      next @numbers << FIRST_STEP_ELEMENT if i == FIRST_STEP_COUNT
-      @numbers << add_pascal_triangle_elements(i, @numbers)
-    end
+    5.time do 
+      puts "あなたの手を選んでください(グー0、チョキ1、パー2)"
+      user_hand = gets
+      cpu_hand = HANDS.keys.sample
 
-    output_results(@numbers)
+      puts "コンピューターは#{HANDS[cpu_hand]}"
+
+    end
   end
-
-  private
-
-    def add_pascal_triangle_elements(add_count, numbers)
-      nested_numbers = []
-      add_count.times do |nested_index|
-        next nested_numbers << FIRST_AND_LAST_STEP_ELEMENT_IN_STEP if nested_index == FIRST_STEP_COUNT
-        next nested_numbers << FIRST_AND_LAST_STEP_ELEMENT_IN_STEP if nested_index == (add_count - PRECEDING_STEP_COUNT)
-
-        nested_numbers << (numbers[add_count - PRECEDING_STEP_COUNT][nested_index - PRECEDING_STEP_COUNT] + numbers[add_count - PRECEDING_STEP_COUNT][nested_index])
-      end
-
-      nested_numbers
-    end
-
-    def output_results(elements)
-      elements.each { |number| $stdout.write("#{number.join(" ")}\n") }
-    end
 end
